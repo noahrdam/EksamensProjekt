508be709ac9437b3e770cc01e56fbba2d0182ef3
noahrdam <noahrdam@gmail.com>
2024-05-22 18:28:31 +0200

    bekræftelse på ansøgning

---
+++ ClientApp/wwwroot/notification.js
@@ -0,0 +1,8 @@
+﻿window.showNotification = (message) => {
+    var notification = document.getElementById("notification");
+    notification.textContent = message || "Ansøgning indsendt med succes!";
+    notification.classList.add("show");
+    setTimeout(function () {
+        notification.classList.remove("show");
+    }, 5000); // Skjuler notifikationen efter 3 sekunder
+};
